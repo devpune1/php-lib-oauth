d8287a828035a1a803bf9c12d8a35ef0ce5ea765
François Kooman <fkooman@tuxed.net>
2015-07-28 13:18:03 +0200

    add NullClientStorage

---
+++ tests/fkooman/OAuth/Storage/NullClientStorageTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace fkooman\OAuth\Storage;
+
+use PHPUnit_Framework_TestCase;
+
+class NullClientStorageTest extends PHPUnit_Framework_TestCase
+{
+    public function testClient()
+    {
+        $clientStorage = new NullClientStorage();
+        $client = $clientStorage->getClient('id', 'code', 'https://example.org/cb', 'foo bar');
+        $this->assertSame('id', $client->getClientId());
+        $this->assertSame('code', $client->getResponseType());
+        $this->assertSame('https://example.org/cb', $client->getRedirectUri());
+        $this->assertSame('foo bar', $client->getScope());
+    }
+
+    public function testMissingRedirectUriClient()
+    {
+        $clientStorage = new NullClientStorage();
+        $this->assertFalse($clientStorage->getClient('id', 'code', null, 'foo bar'));
+    }
+}
